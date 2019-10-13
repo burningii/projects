eb6360032de52cb0813c09a3e9947ab7365c824c
zb1715734811 <1715734811@qq.com>
2019-10-13 13:10:41 +0800

    珠联璧合

---
+++ 珠联璧合婚恋/zlbh_wxapp/siteinfo.js
@@ -0,0 +1,11 @@
+var t = {
+  title: "株联璧合婚恋",
+  uniacid: "32",
+  acid: "1",
+  multiid: "11",
+  version: "1.8",
+  siteroot: "https://thf.ogilvynet.com/app/index.php",
+  token: ''
+};
+
+module.exports = t;
